c8d938af5912f7b5de4e803cd4ca116294b24405
Viktor S <sandorikviktor@gmail.com>
2024-10-03 22:33:51 +0300

    red

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,21 +1,53 @@
-import css from "./Contact.module.css";
-import { FaPhone, FaUser } from "react-icons/fa6";
+import style from "./Contact.module.css";
+// import { FaPhone, FaUser } from "react-icons/fa6";
+import { BsFillPersonFill, BsFillTelephoneFill } from "react-icons/bs";
+import { deleteContact } from "../redux/contactsSlice";
+import { useDispatch } from "react-redux";
 
-export default function Contact({ id, name, number, onDelete }) {
+export default function Contact({ contact }) {
+  const dispatch = useDispatch();
+
+  function handleClick(id) {
+    dispatch(deleteContact(id));
+  }
+
   return (
-    <>
-      <div className={css.info}>
-        <p className={css.p}>
-          <FaUser size="16" />
-          {name}
+    <div className={style.contact}>
+      <div className={style.contactInfo}>
+        <p className={style.contactName}>
+          <BsFillPersonFill />
+          &nbsp;
+          {contact.name}
         </p>
-        <p className={css.p}>
-          <FaPhone size="16" />
-          {number}
+        <p className={style.contactTel}>
+          <BsFillTelephoneFill />
+          &nbsp;
+          <a href={`tel:${contact.number}`}>{contact.number}</a>
         </p>
       </div>
+      <button
+        className={style.contactDeleteBtn}
+        onClick={() => handleClick(contact.id)}
+      >
+        Delete
+      </button>
+    </div>
+  ); 
 
-      <button onClick={() => onDelete(id)}>Delete</button>
-    </>
-  );
+  // return (
+  //   <>
+  //     <div className={css.info}>
+  //       <p className={css.p}>
+  //         <FaUser size="16" />
+  //         {name}
+  //       </p>
+  //       <p className={css.p}>
+  //         <FaPhone size="16" />
+  //         {number}
+  //       </p>
+  //     </div>
+
+  //     <button onClick={() => onDelete(id)}>Delete</button>
+  //   </>
+  // );
 }
--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,28 +1,13 @@
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import { useId } from "react";
 import * as Yup from "yup";
-import css from "./ContactForm.module.css";
+import style from "./ContactForm.module.css";
+import { useDispatch } from "react-redux";
+import { addContact } from "../redux/contactsSlice";
+import { nanoid } from "nanoid";
 
-export default function ContactForm({ onAdd }) {
-  const nameFieldId = useId();
-  const numberFieldId = useId();
-
-  const initialValues = {
-    name: "",
-    number: "",
-  };
-
-  const handleSubmit = (values, actions) => {
-    const newContact = {
-      id: `id-${Date.now()}`,
-      ...values,
-    };
-    onAdd(newContact);
-    actions.resetForm();
-  };
-
-  const validationSchema = Yup.object().shape({
-    name: Yup.string()
+const validationSchema = Yup.object().shape({
+  name: Yup.string()
       .min(3, "is too short!")
       .max(50, "is too long!")
       .matches(/^[A-Za-z\s-]+$/, "must be a valid name!")
@@ -31,33 +16,127 @@ export default function ContactForm({ onAdd }) {
       .min(7, "is too short!")
       .matches(/^[0-9-]+$/, "must be a valid phone number!")
       .required("is required!"),
-  });
+});
 
-  return (
+export default function ContactForm() {
+  const nameFieldId = useId();
+  const numberFieldId = useId();
+  const dispatch = useDispatch();
+
+  function handleSubmit(values, actions) {
+    const contact = {
+      id: nanoid(),
+      name: values.name,
+      number: values.number,
+    };
+    dispatch(addContact(contact));
+    actions.resetForm();
+  }
+
+ return (
     <Formik
-      initialValues={initialValues}
-      onSubmit={handleSubmit}
+      initialValues={{ name: "", number: "" }}
       validationSchema={validationSchema}
+      onSubmit={handleSubmit}
     >
-      <Form className={css.form}>
-        <label className={css.label} htmlFor={nameFieldId}>
-          Name
-          <ErrorMessage name="name">
-            {(msg) => <span className={css.error}>{msg}</span>}
-          </ErrorMessage>
-        </label>
-        <Field type="text" name="name" id={nameFieldId} />
-
-        <label className={css.label} htmlFor={numberFieldId}>
-          Number
-          <ErrorMessage name="number">
-            {(msg) => <span className={css.error}>{msg}</span>}
-          </ErrorMessage>
-        </label>
-        <Field type="text" name="number" id={numberFieldId} />
-
-        <button type="submit">Add contact</button>
+      <Form className={style.form}>
+        <div className={style.fieldWrapper}>
+          <label htmlFor={nameFieldId} className={style.label}>
+            Name<ErrorMessage
+            name="name"
+            component="span"
+            className={style.errorMsg}
+          />
+          </label>
+          <Field
+            name="name"
+            type="text"
+            id={nameFieldId}
+            className={style.field}
+          />
+          
+        </div>
+        <div className={style.fieldWrapper}>
+          <label htmlFor={numberFieldId} className={style.label}>
+            Number<ErrorMessage
+            name="number"
+            component="span"
+            className={style.errorMsg}
+          />
+          </label>
+          <Field
+            name="number"
+            type="text"
+            id={numberFieldId}
+            className={style.field}
+          />
+          
+        </div>
+        <button type="submit" className={style.submitBtn}>
+          Add contact
+        </button>
       </Form>
     </Formik>
   );
 }
+
+
+
+// export default function ContactForm({ onAdd }) {
+//   const nameFieldId = useId();
+//   const numberFieldId = useId();
+
+//   const initialValues = {
+//     name: "",
+//     number: "",
+//   };
+
+//   const handleSubmit = (values, actions) => {
+//     const newContact = {
+//       id: `id-${Date.now()}`,
+//       ...values,
+//     };
+//     onAdd(newContact);
+//     actions.resetForm();
+//   };
+
+//   const validationSchema = Yup.object().shape({
+//     name: Yup.string()
+//       .min(3, "is too short!")
+//       .max(50, "is too long!")
+//       .matches(/^[A-Za-z\s-]+$/, "must be a valid name!")
+//       .required("is required!"),
+//     number: Yup.string()
+//       .min(7, "is too short!")
+//       .matches(/^[0-9-]+$/, "must be a valid phone number!")
+//       .required("is required!"),
+//   });
+
+//   return (
+//     <Formik
+//       initialValues={initialValues}
+//       onSubmit={handleSubmit}
+//       validationSchema={validationSchema}
+//     >
+//       <Form className={css.form}>
+//         <label className={css.label} htmlFor={nameFieldId}>
+//           Name
+//           <ErrorMessage name="name">
+//             {(msg) => <span className={css.error}>{msg}</span>}
+//           </ErrorMessage>
+//         </label>
+//         <Field type="text" name="name" id={nameFieldId} />
+
+//         <label className={css.label} htmlFor={numberFieldId}>
+//           Number
+//           <ErrorMessage name="number">
+//             {(msg) => <span className={css.error}>{msg}</span>}
+//           </ErrorMessage>
+//         </label>
+//         <Field type="text" name="number" id={numberFieldId} />
+
+//         <button type="submit">Add contact</button>
+//       </Form>
+//     </Formik>
+//   );
+// }
